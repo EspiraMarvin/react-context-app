45bd0f723fcc52a3fa46815f55a5e51506e4b702
EspiraMarvin <espiramarvin@gmail.com>
2020-12-16 12:39:12 +0300

    context hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -12,11 +12,11 @@ class AuthContextProvider extends Component {
     };
     render() {
         return (
-            <AuthContext.Provider value={{...this.state, toggleAuth: this.toggleAuth}}>
+            <AuthContext.Provider value={{...this.state, toggleAuth: this.toggleAuth }}>
                 { this.props.children }
             </AuthContext.Provider>
         )
     }
 }
 
-export default AuthContextProvider
+export default AuthContextProvider;
