06c60e858b2e1c88e0d1163d17961d412c0fd43e
EspiraMarvin <espiramarvin@gmail.com>
2020-12-16 09:09:50 +0300

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { ThemeContext } from "../context/ThemeContext";
+
+class Navbar extends Component {
+    static contextType = ThemeContext;
+    render() {
+        return(
+            <ThemeContext.Consumer>{(context) => {
+                console.log('context', context);
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax}}>
+                        <h1>Context app</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                )
+            }}
+            </ThemeContext.Consumer>
+        )
+    }
+}
+
+export default Navbar;
